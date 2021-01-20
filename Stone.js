3d0fd7b689bb35b90674c8ee3379e94769cef6dd
Vasanthan Anbu Devan <75612718+vasanthananbu@users.noreply.github.com>
2021-01-20 17:34:00 +0530

    Update Stone.js

--- Stone.js
+++ Stone.js
@@ -2,9 +2,8 @@ class Stone{
 	constructor(x, y, width, height){
 		var options={
 			restitution: 0.8,
-			friction: 1.0,
-			density: 3.0,
-			mass: 4.0
+			friction: 0.9,
+			density: 1.2,
 		}
 		//properties of Box object
 		this.body = Bodies.rectangle(x, y, width, height, options);
